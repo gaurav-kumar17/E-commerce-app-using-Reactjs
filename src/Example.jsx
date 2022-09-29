650ccdf9fb47e0823e696560fa35661affba9300
gaurav-kumar17 <gk170106@gmail.com>
2022-09-29 14:34:20 +0530

    anothere commit

--- src/Example.jsx
+++ src/Example.jsx
@@ -2,18 +2,32 @@ import { useEffect, useRef, useState } from "react";
 
 const Example=()=>{
     const[text,setText]=useState("");
-    const count=useRef(0); // it return object
+    const previousInputValue=useRef(""); // it return object
 
     useEffect(()=>{
         
-        count.current+=1;
-        console.log(count.current);
-    })
-    // Note:- useeffect return function chalne ke baad chalega
+        // console.log(count.current);
+        // count.current+=1;
+        // const name=document.getElementById("name");
+        // console.log();
+        // name.current.style.color="red"
+        previousInputValue.current=text;
+        console.log(previousInputValue.current);
+    },[text])
+    // Note:- useEffect return function chalne ke baad chalega
+    // ref={name} se pura element ka reference name wale variable mein chala jayega.
     return(
-        <>
-            <input type="text" onChange={(e)=>setText(e.target.value)}></input>
-            <h2>{text}</h2>
+        <>           
+            <input id="name" type="text" onChange={(e)=>setText(e.target.value)}></input>
+            {/* <h2>{name.current}</h2> */}
+            {/* <button onClick={()=>
+                // name.current.focus()
+                name.current.style.color="red"
+            }>Click</button> */}
+
+            <h2>Current Value:{text}</h2>
+            <h2>Current Value:{previousInputValue.current}</h2>
+            
         </>
     )
 }
