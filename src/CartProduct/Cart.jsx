988374952a7ac4cc71eaeb7e8deb66dc50bcd894
gaurav-kumar17 <gk170106@gmail.com>
2022-09-30 19:41:52 +0530

    updated cart-section

--- src/CartProduct/Cart.jsx
+++ src/CartProduct/Cart.jsx
@@ -7,7 +7,20 @@ const Cart=()=>{
     const products=useContext(CartContext);
     console.log(products);
     return(
-        
+        <div className="container pb-4">
+            <div className="row">
+                <div className="col-9">
+                    {products && products.map(()=>{
+                        return <CartProductCart/>;
+                    })}
+                    
+                </div>  
+
+                <div className="col-3">
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 export default Cart;
